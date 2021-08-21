b7e87c6c7588c00960c787c00a4f058efdf061c2
Kimihiro N <kimihiro.nisimura@gmail.com>
2021-08-21 11:04:46 +0900

    Add index (14000)

--- webapp/sql/0_Schema.sql
+++ webapp/sql/0_Schema.sql
@@ -3,35 +3,47 @@ DROP TABLE IF EXISTS `isu_condition`;
 DROP TABLE IF EXISTS `isu`;
 DROP TABLE IF EXISTS `user`;
 
-CREATE TABLE `isu` (
-  `id` bigint AUTO_INCREMENT,
-  `jia_isu_uuid` CHAR(36) NOT NULL UNIQUE,
-  `name` VARCHAR(255) NOT NULL,
-  `image` LONGBLOB,
-  `character` VARCHAR(255),
-  `jia_user_id` VARCHAR(255) NOT NULL,
-  `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
-  `updated_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-   PRIMARY KEY(`id`)
-) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+CREATE TABLE `isu`
+(
+    `id`           bigint AUTO_INCREMENT,
+    `jia_isu_uuid` CHAR(36)     NOT NULL UNIQUE,
+    `name`         VARCHAR(255) NOT NULL,
+    `image`        LONGBLOB,
+    `character`    VARCHAR(255),
+    `jia_user_id`  VARCHAR(255) NOT NULL,
+    `created_at`   DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
+    `updated_at`   DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8mb4;
 
-CREATE TABLE `isu_condition` (
-  `id` bigint AUTO_INCREMENT,
-  `jia_isu_uuid` CHAR(36) NOT NULL,
-  `timestamp` DATETIME NOT NULL,
-  `is_sitting` TINYINT(1) NOT NULL,
-  `condition` VARCHAR(255) NOT NULL,
-  `message` VARCHAR(255) NOT NULL,
-  `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY(`id`)
-) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+CREATE INDEX jia_user_id on isu (jia_user_id);
 
-CREATE TABLE `user` (
-  `jia_user_id` VARCHAR(255) PRIMARY KEY,
-  `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6)
-) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+CREATE TABLE `isu_condition`
+(
+    `id`           bigint AUTO_INCREMENT,
+    `jia_isu_uuid` CHAR(36)     NOT NULL,
+    `timestamp`    DATETIME     NOT NULL,
+    `is_sitting`   TINYINT(1)   NOT NULL,
+    `condition`    VARCHAR(255) NOT NULL,
+    `message`      VARCHAR(255) NOT NULL,
+    `created_at`   DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8mb4;
 
-CREATE TABLE `isu_association_config` (
-  `name` VARCHAR(255) PRIMARY KEY,
-  `url` VARCHAR(255) NOT NULL UNIQUE
-) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+CREATE INDEX jia_isu_uuid on isu_condition (jia_isu_uuid);
+
+CREATE TABLE `user`
+(
+    `jia_user_id` VARCHAR(255) PRIMARY KEY,
+    `created_at`  DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6)
+) ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8mb4;
+
+CREATE TABLE `isu_association_config`
+(
+    `name` VARCHAR(255) PRIMARY KEY,
+    `url`  VARCHAR(255) NOT NULL UNIQUE
+) ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8mb4;
